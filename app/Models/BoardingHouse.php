1f3f01d69ca69ff1ba29545cd9700c5d0671f049
down-glitch <ridw60n@gmail.com>
2026-01-01 19:59:20 +0700

    Initial commit

---
+++ app/Models/BoardingHouse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BoardingHouse extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'city_id',
+        'category_id',
+        'descriprtion',
+        'price',
+        'address'
+    ];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function category()
+    {
+        return $this->belongTo(Category::class);
+    }
+
+    public function rooms()
+    {
+        return $this->hasMany(Room::class);
+    }
+
+    public function bonuses()
+    {
+        return $this->hasMany(Bonus::class);
+    } 
+
+    public function testimonials()
+    {
+        return $this->hasMany(Testimonial::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
+
---
+++ app/Models/Room.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    protected $fillable = [
+        'boarding_House_id',
+        'name',
+        'room_type',
+        'square_feet',
+        'price_per_month',
+        'is_available'
+    ];
+
+    public function boardingHouse()
+    {
+        return $this->belongTo(BoardingHouse::class);
+    }
+
+    public function Images()
+    {
+        return $this->hasMany(RoomImage::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Room::class);
+    }
+}
